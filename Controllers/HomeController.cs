aa5286cdf2f94ab94af29df4127de5faaf69d400
Riverface <sonoskeakigawa@gmail.com>
2020-06-23 10:53:13 -0700

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiCall.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcApiCall.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Article> allArticles = Article.GetArticles("BXjGG7wWhdATCF5wGBWVlAN2elpL6ifp");
+            return View(allArticles);
+        }
+    }
+}
---
+++ Models/Article.cs
@@ -0,0 +1,25 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace MvcApiCall.Models
+{
+    public class Article
+    {
+        public string Section { get; set; }
+        public string Title { get; set; }
+        public string Abstract { get; set; }
+        public string Url { get; set; }
+        public string Byline { get; set; }
+
+        public static List<Article> GetArticles(string apiKey)
+        {
+            var apiCallTask = ApiHelper.ApiCall(apiKey);
+            var result = apiCallTask.Result;
+
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+            List<Article> articleList = JsonConvert.DeserializeObject<List<Article>>(jsonResponse["results"].ToString());
+            return articleList;
+        }
+    }
+}
